091e0ca635789c9167f69c5e78090e07b69fac96
lorenaps <lorena.santpe@gmail.com>
2020-06-11 16:54:53 -0300

    Atualizando campos das structs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dadosjusbr/executor
 
 go 1.13
+
+require github.com/dadosjusbr/storage v0.0.0-20200610184129-9f957c447914
--- pipeline.go
+++ pipeline.go
@@ -1,41 +1,22 @@
-package pipeline
-
-import "github.com/dadosjusbr/storage"
-
-//Env represents the variables used in the stages.
-type Env struct {
-	Month          string
-	Year           string
-	OutputFolder   string
-	MongoURI       string
-	DBName         string
-	MongoMICol     string
-	MongoAgCol     string
-	BackupArtfacts bool
-	SwiftUsername  string
-	SwiftAPIKey    string
-	SwiftAuthURL   string
-	SwiftDomain    string
-	SwiftContainer string
-}
+package executor
 
 //Stage is a phase of data release process.
 type Stage struct {
 	Name string
 	Dir  string
+	Repo string
+	Envs map[string]string
 }
 
 //Pipeline represents the sequence of stages for data release.
 type Pipeline struct {
-	Name   string
-	Path   string
-	Envs   Env
-	Stages []Stage
+	Name        string
+	DefaultRepo string
+	Stages      []Stage
 }
 
-// ProcInfo stores information about a process execution.
-// Note: Replics the struct from https://github.com/dadosjusbr/storage/blob/master/agency.go#L49
-type ProcInfo struct {
+// StageExecutionResult represents information about the execution of a stage.
+type StageExecutionResult struct {
 	Stdin      string   `json:"stdin" bson:"stdin,omitempty"`             // String containing the standard input of the process.
 	Stdout     string   `json:"stdout" bson:"stdout,omitempty"`           // String containing the standard output of the process.
 	Stderr     string   `json:"stderr" bson:"stderr,omitempty"`           // String containing the standard error of the process.
@@ -46,7 +27,7 @@ type ProcInfo struct {
 }
 
 //Run executes the pipeline
-func Run(pipeline Pipeline) ([]storage.ProcInfo, error) {
+func Run(pipeline Pipeline) ([]StageExecutionResult, error) {
 
-	return []ProcInfo{}, nil
+	return []StageExecutionResult{}, nil
 }
