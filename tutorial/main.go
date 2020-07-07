6f084823f8b70fa026cd0b54effee2dadf6569c9
lorenaps <lorena.santpe@gmail.com>
2020-07-07 15:07:23 -0300

    Pegando último commit

--- tutorial/main.go
+++ tutorial/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/dadosjusbr/executor"
 )
 
 func main() {
 	goPath := os.Getenv("GOPATH")
+	repo := fmt.Sprintf("%s/src/github.com/dadosjusbr/coletores", goPath)
+
+	cmdList := strings.Split("git rev-list -1 HEAD", " ")
+	cmd := exec.Command(cmdList[0], cmdList[1:]...)
+	cmd.Dir = repo
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
 	collectBuildEnv := map[string]string{
-		"GIT_COMMIT": "9bdeec22238644fa78ff7e3e9ab6f126fcaefd29",
+		"GIT_COMMIT": out.String(),
 	}
 
 	p := executor.Pipeline{}
-	p.Name = "mppb"
-	p.DefaultRepo = fmt.Sprintf("%s/src/github.com/dadosjusbr/coletores", goPath)
+	p.Name = "MPPB"
+	p.DefaultRepo = repo
 	p.Stages = []executor.Stage{
 		{
 			Name:     "Coleta",
