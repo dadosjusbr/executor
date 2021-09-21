08d483e866d6ec3846f3e951a686df066a8663b8
Daniel Fireman <danielfireman@gmail.com>
2021-09-21 11:55:21 -0300

    Adicionando linha de comando para o executor

---
+++ cmd/go.mod
@@ -0,0 +1,7 @@
+module executor
+
+go 1.16
+
+replace github.com/dadosjusbr/executor => /home/daniel/repos/dadosjubsr/executor
+
+require github.com/dadosjusbr/executor v1.0.0
---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/dadosjusbr/executor"
+)
+
+func main() {
+	in, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("Erro lendo dados da entrada padrão: %q", err)
+	}
+
+	fmt.Printf("%+v\n\n", string(in))
+
+	var p executor.Pipeline
+	if err := json.Unmarshal(in, &p); err != nil {
+		log.Fatalf("Erro convertendo pipeline da entrada padrão: %q\n\"%s\"", err, string(in))
+	}
+
+	log.Printf("Executando pipeline %s", p.Name)
+	result, err := p.Run()
+	if err != nil {
+		log.Fatalf("Erro executando pipeline: %q\n\"%+v\"", err, p)
+	}
+	log.Printf("Pipeline %s executado com sucesso!", p.Name)
+	fmt.Printf("%+v", result)
+}
