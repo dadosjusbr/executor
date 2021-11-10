86fce37c02e966f4919e39137c1ed78d0df15242
Daniel Fireman <danielfireman@gmail.com>
2021-11-10 14:58:19 +0000

    adicionando mapa de flags passado pela linha de comando do executor

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 
 	"github.com/dadosjusbr/executor"
 	"github.com/dadosjusbr/executor/status"
+	"github.com/spf13/pflag"
 )
 
 var (
-	input = flag.String("in", "", "Path for the descriptor file.")
+	input          = pflag.String("in", "", "Path for the descriptor file.")
+	defaultEnvFlag = pflag.StringSlice("def-run-env", []string{}, "Environment variables that override the default vars.")
 )
 
 func main() {
-	flag.Parse()
+	pflag.Parse()
+
+	defaultEnv := make(map[string]string)
+	for _, e := range *defaultEnvFlag {
+		env := strings.Split(e, ":")
+		if len(env) != 2 {
+			log.Fatalf("Invalid env var spec: %s", e)
+		}
+		defaultEnv[env[0]] = env[1]
+	}
 
 	if *input == "" {
 		log.Fatal("Path to the input file not found. Forgot --in?")
@@ -32,6 +43,7 @@ func main() {
 		log.Fatalf("Erro convertendo pipeline da entrada padrão: %q\n\"%s\"", err, string(in))
 	}
 
+	p.DefaultRunEnv = mergeMaps(p.DefaultRunEnv, defaultEnv) // merging maps.
 	log.Printf("Pipeline: %+v\n\n", p)
 
 	log.Printf("Executando pipeline %s", p.Name)
@@ -43,4 +55,19 @@ func main() {
 	}
 	log.Printf("Pipeline %s executado com sucesso! Imprimindo resultado:\n\n", p.Name)
 	fmt.Printf("%+v", result)
+}
+
+// mergeMaps adds all elements of sec to first.
+func mergeMaps(first, sec map[string]string) map[string]string {
+	if first == nil {
+		return sec
+	}
+	env := make(map[string]string)
+	for k, v := range first {
+		env[k] = v
+	}
+	for k, v := range sec {
+		env[k] = v
+	}
+	return env
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/go-git/go-git/v5 v5.4.2
+	github.com/spf13/pflag v1.0.5
 	google.golang.org/protobuf v1.27.1
 )
